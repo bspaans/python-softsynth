e3c4c7d01f35cb85057599cb95677f7dc4ccf76f
Bart Spaans <bspaans@bird.localdomain>
2014-12-25 06:55:07 +0100

    BS: Got oscillators working as well; and improved performance yet again

--- synth/envelopes.py
+++ synth/envelopes.py
@@ -1,9 +1,16 @@
+import numpy
+import sys
 
 class ConstantFrequencyEnvelope(object):
     def __init__(self, freq):
         self.freq = freq
     def get_frequency(self, options, t):
         return self.freq
+    def get_frequencies(self, phase, nr_of_samples):
+        result = numpy.empty([nr_of_samples])
+        result.fill(self.freq)
+        return result
+        
 
 class BendFrequencyEnvelope(object):
     def __init__(self, start_freq, end_freq, bend_time = 44100):
@@ -21,10 +28,49 @@ class ConstantAmplitudeEnvelope(object):
         self.max_amplitude = max_amplitude
     def get_amplitude(self, options, t):
         return self.max_amplitude
+    def get_amplitudes(self, phase, nr_of_samples):
+        return numpy.full([nr_of_samples], self.max_amplitude)
+    
+class SegmentAmplitudeEnvelope(object):
+    def __init__(self):
+        self.segments = []
+        self.last_level = 0.0
+        self.last_position = 0
+
+    def add_segment(self, level, duration):
+        segment_range = level - self.last_level
+        step_size = segment_range / duration
+        arr = numpy.full([duration], step_size).cumsum()
+        arr = numpy.add(arr, self.last_level)
+        self.segments.append((self.last_position, self.last_position + duration, level, arr))
+        self.last_position += duration
+        self.last_level = level
+
+    def get_amplitudes(self, phase, nr_of_samples):
+        result = numpy.empty([nr_of_samples])
+        result_length = 0
+        last_level = 0.0
+        for (start, stop, level, arr) in self.segments:
+            if phase >= start and phase < stop:
+                index = phase - start
+                max_length = stop - index
+                if max_length >= nr_of_samples - result_length:
+                    result[result_length:nr_of_samples] = arr[index:index+nr_of_samples - result_length]
+                else:
+                    result[result_length:result_length + max_length] = arr[index:]
+                result_length += max_length
+            if result_length >= nr_of_samples:
+                return result
+            last_level = level
+
+        if result_length < nr_of_samples:
+            result[result_length:] = last_level
+        return result
 
 class ADSRAmplitudeEnvelope(object):
     def __init__(self, max_amplitude = 1.0):
         self.max_amplitude = max_amplitude
+        self.segment_envelope = None
         self.set_attack(0.01)
         self.set_decay(0.1)
         self.set_sustain(0.0)
@@ -48,4 +94,16 @@ class ADSRAmplitudeEnvelope(object):
             return self.sustain_level
         release_time = options.sample_rate * float(self.release)
         return self.sustain_level - ((t - stopped_since) / release_time) * self.sustain_level
+
+    def get_segment_envelope(self, options):
+        if self.segment_envelope is not None:
+            return self.segment_envelope
+        s = SegmentAmplitudeEnvelope()
+        s.add_segment(self.max_amplitude, float(self.attack) * options.sample_rate)
+        s.add_segment(self.sustain_level, float(self.delay) * options.sample_rate)
+        self.segment_envelope = s
+        return s
+
+    def get_amplitudes(self, phase, nr_of_samples):
+        return self.get_segment_envelope().get_amplitudes(phase, nr_of_samples)
 
--- synth/oscillator.py
+++ synth/oscillator.py
@@ -5,33 +5,38 @@ import sys
 import cProfile
 import pstats
 import numpy
+import envelopes
 
-class SynthObject(object):
-    def __init__(self, options):
+class Oscillator(object):
+    def __init__(self, options, amplitude_envelope = None):
         self.options = options
         self.freq = options.pitch_standard
         self.phase = 0
+        self.phase_incr = self.options.two_pi_divided_by_sample_rate * self.freq
+        self.amp_env = amplitude_envelope
 
     def get_samples(self, nr_of_samples):
-        phase_incr = self.options.two_pi_divided_by_sample_rate * self.freq
-        result = numpy.arange(nr_of_samples)
-        result = numpy.multiply(result, phase_incr)
-        result = numpy.add(result, self.phase * phase_incr)
+        result = numpy.full([nr_of_samples], self.phase_incr).cumsum()
+        result = numpy.add(result, self.phase * self.phase_incr)
         result = numpy.sin(result)
+        if self.amp_env is not None:
+            amplitudes = self.amp_env.get_amplitudes(self.phase, nr_of_samples)
+            result = numpy.multiply(result, amplitudes)
         self.phase += nr_of_samples
+        return result
+
+    def get_samples_in_byte_rate(self, nr_of_samples):
+        result = self.get_samples(nr_of_samples)
         return numpy.multiply(result, self.options.max_value).astype(int)
-
-class Oscillator(SynthObject):
-    def __init__(self, options):
-        super(Oscillator, self).__init__(options)
-
-    def phase_to_int(self, phase):
-        return math.sin(phase)
-
 
 
 def profile_call():
-    Oscillator(options.Options()).get_samples(44100)
+    opts = options.Options()
+    amp_env = envelopes.SegmentAmplitudeEnvelope()
+    amp_env.add_segment(1.0, 20000)
+    amp_env.add_segment(0.0, 20000)
+    osc = Oscillator(opts, amp_env)
+    osc.get_samples_in_byte_rate(44100)
 
 def profile():
     if "--profile" in sys.argv:
@@ -44,11 +49,15 @@ def main():
     profile()
     t= 0
     opts = options.Options()
-    osc = Oscillator(opts)
+    amp_env = envelopes.SegmentAmplitudeEnvelope()
+    amp_env.add_segment(1.0, 20000)
+    amp_env.add_segment(0.2, 20000)
+    amp_env.add_segment(0.0, 10000)
+    osc = Oscillator(opts, amp_env)
     wave = wave_writer.WaveWriter(opts, "output.wav", also_output_to_stdout = True)
     try:
         while True:
-            wave.write_samples(osc.get_samples(1000))
+            wave.write_samples(osc.get_samples_in_byte_rate(1000))
             t += 1000
     except KeyboardInterrupt:
         print "Written", t, "samples" 
