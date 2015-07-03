3caa763019784062e7d81713141ec5412b72c7c6
Bart Spaans <bart.spaans@gmail.com>
2015-07-03 20:58:11 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("setuptools is missing. Install it using your package "
+        + "manager (usually python-setuptools) or via pip (pip"
+        + " install setuptools).")
+
+setup(name= "softsynth",
+    version = "0.1.0",
+    description = "Proof of concept software synthesizer",
+    long_description = """you too""",
+    author = "Bart Spaans",
+    author_email = "bart.spaans@gmail.com",
+    url = "https://github.com/bspaans/python-softsynth",
+    packages = ['synth', 'synth.midi', 'synth.instruments'],
+    license="GPLv3",
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Artistic Software',
+        'Topic :: Education',
+        'Topic :: Multimedia',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: MIDI',
+        ],
+    entry_points = { 
+        'console_scripts': [
+            'synth = synth.main:main'
+        ]
+    }
+)
